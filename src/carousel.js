3a5cb2d9db441850930bd7b3af69ea06f2f8660a
chilijung <chilijung@gmail.com>
2017-07-18 14:28:01 +0800

    replace TouchableNativeFeedback with TouchableOpacity, because it works strangely in android.

--- src/carousel.js
+++ src/carousel.js
@@ -16,24 +16,52 @@ type Props = {
 
 export default class Carosuel extends Component {
   props: Props
+  scrolling: boolean
+
+  constructor(props: Props) {
+    super(props);
+
+    this.scrolling = false;
+    (this: any)._onScroll = this._onScroll.bind(this);
+    (this: any)._setScrollFalse = this._setScrollFalse.bind(this);
+    (this: any)._setPageChange = this._setPageChange.bind(this);
+  }
+
   shouldComponentUpdate(nextProps: Props) {
     return !nextProps.fromCarousel;
   }
 
+  _onScroll() {
+    this.scrolling = true;
+  }
+
+  _setScrollFalse() {
+    this.scrolling = false;
+  }
+
+  _setPageChange(activeIndex) {
+    const {onPageChange} = this.props;
+    this._setScrollFalse();
+    onPageChange(activeIndex, true);
+  }
+
   render() {
-    const {onPressImage, onPageChange, images, ...rest} = this.props;
+    const {onPressImage, images, ...rest} = this.props;
     return (
       <Carousel
         {...rest}
-        onPageChange={(activeIndex) => onPageChange(activeIndex, true)}
+        onPageChange={this._setPageChange}
+        onScroll={this._onScroll}
+        onScrollBegin={this._setScrollFalse}
         >
         {
+          /* this.scrolling prevent trigger onPress while is scrolling */
           images.map((img, i) => {
             return (
               <TouchableImage
                 key={i}
                 image={img}
-                onPress={() => onPressImage(i)}
+                onPress={this.scrolling ? () => {} : () => onPressImage(i)}
                 />
             );
           })
